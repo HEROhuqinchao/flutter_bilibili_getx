a561401b2fca0a2ee29f736cdc519e94c430bf89
中国人二号 <2622504692@qq.com>
2022-09-14 20:10:22 +0800

    上下滑动调节视频的亮度和音量

--- lib/ui/pages/video_play/bilibili_video_player/bilibili_video_player_logic.dart
+++ lib/ui/pages/video_play/bilibili_video_player/bilibili_video_player_logic.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 import 'dart:ui';
 
+import 'package:brightness_volume/brightness_volume.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:video_player/video_player.dart';
 
@@ -15,8 +17,7 @@ class BilibiliVideoPlayerLogic extends GetxController {
   void onInit() {
     state.videoPlayerController = VideoPlayerController.network(
         // "http://61.164.90.254:9000/dm-pls/08388d26a77a413fa8da09837c6df420.mp4"
-        "https://media.w3.org/2010/05/sintel/trailer.mp4"
-    )
+        "https://media.w3.org/2010/05/sintel/trailer.mp4")
       ..initialize().then((value) {
         state.latestValue = state.videoPlayerController.value;
         update();
@@ -25,6 +26,9 @@ class BilibiliVideoPlayerLogic extends GetxController {
       state.latestValue = state.videoPlayerController.value;
       update();
     });
+
+    ///获取音量和亮度
+    fetchVolumeBrightness();
     super.onInit();
   }
 
@@ -35,6 +39,12 @@ class BilibiliVideoPlayerLogic extends GetxController {
     });
     state.videoPlayerController.dispose();
     super.onClose();
+  }
+
+  ///获取音量和亮度
+  void fetchVolumeBrightness() async {
+    state.brightness = (await BVUtils.brightness).clamp(0.0, 1.0);
+    state.volume = (await BVUtils.volume).clamp(0.0, 1.0);
   }
 
   ///播放或暂停
@@ -54,6 +64,8 @@ class BilibiliVideoPlayerLogic extends GetxController {
 
   ///水平拖动进度条
   void videoPlayProgressOnHorizontalDragStart() {
+    ///是否显示视频当前进度的弹框（拖动进度条时显示）
+    state.videoProgress = true;
     if (!state.videoPlayerController.value.isInitialized) {
       return;
     }
@@ -77,6 +89,7 @@ class BilibiliVideoPlayerLogic extends GetxController {
 
   ///拖动结束
   void videoPlayProgressOnHorizontalDragEnd() {
+    state.videoProgress = false;
     if (state.controllerWasPlaying) {
       state.videoPlayerController.play();
     }
@@ -95,7 +108,7 @@ class BilibiliVideoPlayerLogic extends GetxController {
   ///隐藏进度条
   void startHideTimer() {
     ///过一段时间隐藏掉
-    state.hideTimer = Timer(const Duration(milliseconds: 2500), () {
+    state.hideTimer = Timer(const Duration(milliseconds: 4000), () {
       state.showBottomBar = false;
       update();
     });
@@ -103,10 +116,12 @@ class BilibiliVideoPlayerLogic extends GetxController {
 
   ///重启隐藏计时器
   void cancelAndRestartTimer() {
-    state.hideTimer.cancel();
-    state.showBottomBar = true;
-    update();
-    startHideTimer();
+    if(state.videoPlayerController.value.isPlaying) {
+      state.hideTimer.cancel();
+      state.showBottomBar = true;
+      update();
+      startHideTimer();
+    }
   }
 
   ///跳转至某一时刻
@@ -145,6 +160,56 @@ class BilibiliVideoPlayerLogic extends GetxController {
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
         overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom]);
+    update();
+  }
+
+  ///开始调节音量
+  void videoPlayVolumeBrightnessOnVerticalDragStart(DragStartDetails details) {
+    print(details.globalPosition.dx);
+    print(1.sw);
+    if (state.isFullScreen) {
+      if (details.globalPosition.dy > 1.sh / 2) {
+        state.videoVolume = true;
+      } else {
+        state.videoBrightness = true;
+      }
+    } else {
+      if (details.globalPosition.dx > 1.sw / 2) {
+        state.videoVolume = true;
+      } else {
+        state.videoBrightness = true;
+      }
+    }
+    update();
+  }
+
+  ///结束调节音量
+  void videoPlayVolumeBrightnessOnVerticalDragEnd() {
+    state.videoBrightness = false;
+    state.videoVolume = false;
+    update();
+  }
+
+  ///音量更新
+  void videoPlayVolumeBrightnessOnVerticalDragUpdate(
+      DragUpdateDetails details) {
+    if (state.isFullScreen) {
+      if (details.globalPosition.dy > 1.sh / 2) {
+        state.volume = (state.volume - (details.delta.dx / 30));
+        BVUtils.setVolume(state.volume);
+      } else {
+        state.brightness = (state.brightness - (details.delta.dx / 30));
+        BVUtils.setBrightness(state.brightness);
+      }
+    } else {
+      if (details.globalPosition.dx > 1.sw / 2) {
+        state.volume = (state.volume - (details.delta.dy / 30));
+        BVUtils.setVolume(state.volume);
+      } else {
+        state.brightness = (state.brightness - (details.delta.dy / 30));
+        BVUtils.setBrightness(state.brightness);
+      }
+    }
     update();
   }
 }
--- lib/ui/pages/video_play/bilibili_video_player/bilibili_video_player_state.dart
+++ lib/ui/pages/video_play/bilibili_video_player/bilibili_video_player_state.dart
@@ -1,24 +1,55 @@
 import 'dart:async';
 
+import 'package:brightness_volume/brightness_volume.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:video_player/video_player.dart';
 import 'dart:ui' as ui;
 
 class BilibiliVideoPlayerState {
   late VideoPlayerController videoPlayerController;
   late bool showBottomBar;
+
   ///进度条顶端图片
   late ui.Image image;
+
+  ///是否在播放
   late bool controllerWasPlaying;
+
+  ///是否在拖动
   late bool dragging;
+
+  ///隐藏计时器
   late Timer hideTimer;
+
+  ///视频最新的状态
   late VideoPlayerValue latestValue;
+
+  ///是否为全屏显示
   late bool isFullScreen;
+
+  ///是否显示视频进度
+  late bool videoProgress;
+
+  ///是否显示视频音量
+  late bool videoVolume;
+
+  ///是否显示视频亮度
+  late bool videoBrightness;
+
+  ///当前音量数值
+  double volume = 0.0;
+
+  ///当前亮度数值
+  double brightness = 0.0;
 
   BilibiliVideoPlayerState() {
     showBottomBar = true;
     controllerWasPlaying = false;
     dragging = false;
     isFullScreen = false;
-    hideTimer = Timer(const Duration(microseconds: 2500), () {});
+    hideTimer = Timer(const Duration(microseconds: 4000), () {});
+    videoProgress = false;
+    videoVolume = false;
+    videoBrightness = false;
   }
 }
