4fa735e100934b6869bbdf69ac4f12610a060365
中国人二号 <oncwnuFHP-600UbXnCzwKw61bv8s@git.weixin.qq.com>
2022-10-29 08:58:58 +0800

    QQ分享（在QqTestView）

--- lib/ui/pages/qq_test/qq_test_logic.dart
+++ lib/ui/pages/qq_test/qq_test_logic.dart
@@ -1,4 +1,5 @@
 import 'package:bilibili_getx/core/qq_util/qq_util.dart';
+import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 
 import 'qq_test_state.dart';
@@ -8,6 +9,14 @@ class QqTestLogic extends GetxController {
 
   @override
   void onReady() {
+    QQUtil.checkQQInstalled().then((value) {
+      if (value == true) {
+        QQUtil.initPermissionGranted();
+        QQUtil.registerApp();
+      } else {
+        SmartDialog.showToast("请先安装QQ");
+      }
+    });
     super.onReady();
   }
 
