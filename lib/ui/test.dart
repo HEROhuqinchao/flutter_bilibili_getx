8c1780c5082d83d201d668797f3298b0cf347a49
中国人二号 <oncwnuFHP-600UbXnCzwKw61bv8s@git.weixin.qq.com>
2023-04-12 16:59:52 +0800

    flutter生命周期

--- lib/ui/test.dart
+++ lib/ui/test.dart
@@ -2,6 +2,7 @@ import 'package:bilibili_getx/ui/shared/app_theme.dart';
 import 'package:bilibili_getx/ui/shared/image_asset.dart';
 import 'package:bilibili_getx/ui/widgets/fade_image_default.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() => runApp(MyApp());
@@ -19,23 +20,87 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Stack"),
         ),
-        body: Stack(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.all(Radius.circular(10)),
-              child: Container(
-                color: HYAppTheme.norGray04Color,
-                child: DefaultFadeImage(
-                  imageUrl:
-                      "http://i0.hdslb.com/bfs/bangumi/image/2719084adaed21fd49d9fd9ecd2fc7b304a06b3a.png",
-                  height: 200,
-                ),
-              ),
-            ),
-            Text("data"),
-          ],
-        ),
+        body: LiveCycleWidget(),
       ),
     );
   }
 }
+
+class LiveCycleWidget extends StatefulWidget {
+  const LiveCycleWidget({Key? key}) : super(key: key);
+
+  @override
+  State<LiveCycleWidget> createState() {
+    print("createState");
+    return _LiveCycleWidgetState();
+  }
+}
+
+class _LiveCycleWidgetState extends State<LiveCycleWidget> {
+  _LiveCycleWidgetState() {
+    print("构造函数");
+  }
+
+  List<Widget> children = [];
+
+  @override
+  void initState() {
+    print("initState()");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print("didChangeDependencies");
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(covariant LiveCycleWidget oldWidget) {
+    print("didUpdateWidget");
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("build");
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            children.add(Container(
+              child: Text("data"),
+            ));
+            setState(() {});
+          },
+          child: Text("点击"),
+        ),
+        ...children,
+      ],
+    );
+  }
+}
+
+// class BasicMessageExample extends StatefulWidget {
+//   const BasicMessageExample({Key? key}) : super(key: key);
+//
+//   @override
+//   State<BasicMessageExample> createState() => _BasicMessageExampleState();
+// }
+//
+// class _BasicMessageExampleState extends State<BasicMessageExample> {
+//   static const _channel =
+//       BasicMessageChannel('basic_message_001', JSONMessageCodec());
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () async {
+//           final String reply = await _channel.send('Hello World') as String;
+//           print(reply);
+//         },
+//       ),
+//     );
+//   }
+// }
