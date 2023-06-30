fbf070206df5ac69cf45ccea352a1cb0c2229811
中国人二号 <oncwnuFHP-600UbXnCzwKw61bv8s@git.weixin.qq.com>
2023-06-30 17:22:32 +0800

    更新

--- lib/ui/pages/functions/animation_study/part014/part014.dart
+++ lib/ui/pages/functions/animation_study/part014/part014.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 
@@ -60,21 +59,22 @@ class _ClockWidgetState extends State<ClockWidget>
         debugDumpRenderTree();
       },
       child: Stack(
+        alignment: Alignment.center,
         children: [
           CustomPaint(
-            size: Size(widget.radius * 2, widget.radius * 2),
-            painter: ClockPainter(
+            painter: ClockBgPainter(
               radius: 100,
-              time: time,
+              // painter: ClockBgPainter(),
             ),
-            // painter: ClockBgPainter(),
           ),
           RepaintBoundary(
             child: CustomPaint(
-              painter: ClockBgPainter(
+              size: Size(widget.radius * 2, widget.radius * 2),
+              painter: ClockPainter(
                 radius: 100,
-                // painter: ClockBgPainter(),
+                time: time,
               ),
+              // painter: ClockBgPainter(),
             ),
           ),
         ],
@@ -85,93 +85,70 @@ class _ClockWidgetState extends State<ClockWidget>
 
 class ClockBgPainter extends CustomPainter {
   final Paint _paint = Paint()..style = PaintingStyle.stroke;
+
   final double radius;
-  final Paint arcPaint = Paint()
-    ..style = PaintingStyle.fill
-    ..color = Color(0xff00abf2);
-  final TextPainter _textPainter = TextPainter(
-    textAlign: TextAlign.center,
-    textDirection: TextDirection.ltr,
-  );
-  final Paint hourPaint = Paint()
-    ..color = Color(0xff00ff00)
-    ..strokeWidth = 5;
-  final Paint minusPaint = Paint()
-    ..color = Color(0xffff00ff)
-    ..strokeWidth = 3;
-  final Paint secondPaint = Paint()
-    ..color = Color(0xff00ffff)
-    ..strokeWidth = 2;
 
-  double get logic1 => radius * 0.01;
-
-  double get minusLen => logic1 * 60; // 分针长
-  double get hourLen => logic1 * 45; // 时针长
-  double get secondLen => logic1 * 68; // 秒针长
-  double get hourLineWidth => logic1 * 3; // 时针线宽
-  double get minusLineWidth => logic1 * 2; // 分针线宽
-  double get secondLineWidth => logic1; // 秒针线宽
-  double get scaleSpace => logic1 * 11; // 刻度与外圈的间隔
-  double get shortScaleLen => logic1 * 7; // 短刻度线长
-  double get shortLenWidth => logic1; // 短刻度线长
-  double get longScaleLen => logic1 * 11; // 长刻度线长
-  double get longLineWidth => logic1 * 2; // 长刻度线宽
-
-  ClockBgPainter({
-    required this.radius,
-  });
+  ClockBgPainter({this.radius = 100});
 
   @override
   void paint(Canvas canvas, Size size) {
+    print("刷新${DateTime.now().toIso8601String()}");
     canvas.translate(size.width / 2, size.height / 2);
     drawOuterCircle(canvas);
+    drawScale(canvas);
+    drawText(canvas);
   }
 
   @override
-  bool shouldRepaint(ClockPainter oldDelegate) {
+  bool shouldRepaint(covariant ClockBgPainter oldDelegate) {
     return oldDelegate.radius != radius;
   }
 
   void drawOuterCircle(Canvas canvas) {
     _paint
       ..strokeWidth = 4
-      ..color = Color(0xffD5D5);
-    _painArc(canvas);
+      ..color = const Color(0xffD5D5D5);
     for (int i = 0; i < 4; i++) {
-      _painArc(canvas);
+      _paintArc(canvas);
       canvas.rotate(pi / 2);
     }
-    drawScale(canvas);
-    drawText(canvas);
   }
 
-  void _painArc(Canvas canvas) {
+  final Paint arcPaint = Paint()
+    ..style = PaintingStyle.fill
+    ..color = const Color(0xff00abf2);
+
+  void _paintArc(Canvas canvas) {
     arcPaint.maskFilter = MaskFilter.blur(BlurStyle.solid, logic1);
     final Path circlePath = Path()
       ..addArc(
-        Rect.fromCenter(
-            center: Offset.zero, width: radius * 2, height: radius * 2),
-        pi / 18,
-        pi / 2 - pi / 9,
-      );
+          Rect.fromCenter(
+              center: Offset(0, 0), width: radius * 2, height: radius * 2),
+          10 / 180 * pi,
+          pi / 2 - 20 / 180 * pi);
+
     Path circlePath2 = Path()
       ..addArc(
-        Rect.fromCenter(
-            center: Offset(-logic1, 0), width: radius * 2, height: radius * 2),
-        10 / 180 * pi,
-        pi / 2 - 20 / 180 * pi,
-      );
+          Rect.fromCenter(
+              center: Offset(-logic1, 0),
+              width: radius * 2,
+              height: radius * 2),
+          10 / 180 * pi,
+          pi / 2 - 20 / 180 * pi);
+    //联合路径
     Path result =
         Path.combine(PathOperation.difference, circlePath, circlePath2);
-    canvas.drawPath(result, arcPaint);
+    canvas.drawPath(result, arcPaint); //绘制
   }
 
   void drawScale(Canvas canvas) {
     _paint
       ..strokeCap = StrokeCap.round
       ..style = PaintingStyle.fill;
+
     double count = 60;
     double perAngle = 2 * pi / count;
+
     for (int i = 0; i < count; i++) {
       if (i % 5 == 0) {
         _paint
@@ -194,29 +171,56 @@ class ClockBgPainter extends CustomPainter {
     }
   }
 
+  double get logic1 => radius * 0.01;
+
+  // 刻度与外圈的间隔
+  double get scaleSpace => logic1 * 11;
+
+  // 短刻度线长
+  double get shortScaleLen => logic1 * 7;
+
+  // 短刻度线长
+  double get shortLenWidth => logic1;
+
+  // 长刻度线长
+  double get longScaleLen => logic1 * 11;
+
+  // 长刻度线宽
+  double get longLineWidth => logic1 * 2;
+
+  final TextPainter _textPainter = TextPainter(
+      textAlign: TextAlign.center, textDirection: TextDirection.ltr);
+
   void drawText(Canvas canvas) {
-    _drawCircleText(canvas, 'IX', offsetX: -radius);
-    _drawCircleText(canvas, 'III', offsetX: radius);
-    _drawCircleText(canvas, 'VI', offsetY: radius);
-    _drawCircleText(canvas, 'XII', offsetY: -radius);
+    _drawCircleText(canvas, 'Ⅸ', offsetX: -radius);
+    _drawCircleText(canvas, 'Ⅲ', offsetX: radius);
+    _drawCircleText(canvas, 'Ⅵ', offsetY: radius);
+    _drawCircleText(canvas, 'Ⅻ', offsetY: -radius);
+    _drawLogoText(canvas, offsetY: -radius * 0.5);
   }
 
   _drawCircleText(Canvas canvas, String text,
       {double offsetX = 0, double offsetY = 0}) {
     _textPainter.text = TextSpan(
-      text: text,
-      style: TextStyle(
-        fontSize: radius * .15,
-        color: Colors.blue,
-      ),
-    );
+        text: text,
+        style: TextStyle(fontSize: radius * 0.15, color: Colors.blue));
     _textPainter.layout();
     _textPainter.paint(
         canvas,
-        Offset.zero.translate(
-          -_textPainter.size.width / 2 + offsetX,
-          -_textPainter.size.height / 2 + offsetY,
-        ));
+        Offset.zero.translate(-_textPainter.size.width / 2 + offsetX,
+            -_textPainter.height / 2 + offsetY));
+  }
+
+  _drawLogoText(Canvas canvas, {double offsetX = 0, double offsetY = 0}) {
+    _textPainter.text = TextSpan(
+        text: 'Toly',
+        style: TextStyle(
+            fontSize: radius * 0.2, color: Colors.blue, fontFamily: 'CHOPS'));
+    _textPainter.layout();
+    _textPainter.paint(
+        canvas,
+        Offset.zero.translate(-_textPainter.size.width / 2 + offsetX,
+            -_textPainter.height / 2 + offsetY));
   }
 }
 
@@ -272,91 +276,7 @@ class ClockPainter extends CustomPainter {
   }
 
   void drawOuterCircle(Canvas canvas) {
-    // _paint
-    //   ..strokeWidth = 4
-    //   ..color = Color(0xffD5D5);
-    // _painArc(canvas);
-    // for (int i = 0; i < 4; i++) {
-    //   _painArc(canvas);
-    //   canvas.rotate(pi / 2);
-    // }
-    // drawScale(canvas);
-    // drawText(canvas);
     drawArrow(canvas, time.value);
-  }
-
-  void _painArc(Canvas canvas) {
-    arcPaint.maskFilter = MaskFilter.blur(BlurStyle.solid, logic1);
-    final Path circlePath = Path()
-      ..addArc(
-        Rect.fromCenter(
-            center: Offset.zero, width: radius * 2, height: radius * 2),
-        pi / 18,
-        pi / 2 - pi / 9,
-      );
-    Path circlePath2 = Path()
-      ..addArc(
-        Rect.fromCenter(
-            center: Offset(-logic1, 0), width: radius * 2, height: radius * 2),
-        10 / 180 * pi,
-        pi / 2 - 20 / 180 * pi,
-      );
-    Path result =
-        Path.combine(PathOperation.difference, circlePath, circlePath2);
-    canvas.drawPath(result, arcPaint);
-  }
-
-  void drawScale(Canvas canvas) {
-    _paint
-      ..strokeCap = StrokeCap.round
-      ..style = PaintingStyle.fill;
-    double count = 60;
-    double perAngle = 2 * pi / count;
-    for (int i = 0; i < count; i++) {
-      if (i % 5 == 0) {
-        _paint
-          ..strokeWidth = longLineWidth
-          ..color = Colors.blue;
-        canvas.drawLine(Offset(radius - scaleSpace, 0),
-            Offset(radius - scaleSpace - longScaleLen, 0), _paint);
-        canvas.drawCircle(
-            Offset(radius - scaleSpace - longScaleLen - logic1 * 5, 0),
-            longLineWidth,
-            _paint..color = Colors.orange);
-      } else {
-        _paint
-          ..strokeWidth = shortLenWidth
-          ..color = Colors.black;
-        canvas.drawLine(Offset(radius - scaleSpace, 0),
-            Offset(radius - scaleSpace - shortScaleLen, 0), _paint);
-      }
-      canvas.rotate(perAngle);
-    }
-  }
-
-  void drawText(Canvas canvas) {
-    _drawCircleText(canvas, 'IX', offsetX: -radius);
-    _drawCircleText(canvas, 'III', offsetX: radius);
-    _drawCircleText(canvas, 'VI', offsetY: radius);
-    _drawCircleText(canvas, 'XII', offsetY: -radius);
-  }
-
-  _drawCircleText(Canvas canvas, String text,
-      {double offsetX = 0, double offsetY = 0}) {
-    _textPainter.text = TextSpan(
-      text: text,
-      style: TextStyle(
-        fontSize: radius * .15,
-        color: Colors.blue,
-      ),
-    );
-    _textPainter.layout();
-    _textPainter.paint(
-        canvas,
-        Offset.zero.translate(
-          -_textPainter.size.width / 2 + offsetX,
-          -_textPainter.size.height / 2 + offsetY,
-        ));
   }
 
   void drawArrow(Canvas canvas, DateTime time) {
