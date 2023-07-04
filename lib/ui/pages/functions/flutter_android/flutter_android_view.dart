eb0acadaca8ae36f30706dffa896b5b834ea40be
中国人二号 <oncwnuFHP-600UbXnCzwKw61bv8s@git.weixin.qq.com>
2023-07-04 08:07:51 +0800

    更新

--- lib/ui/pages/functions/flutter_android/flutter_android_view.dart
+++ lib/ui/pages/functions/flutter_android/flutter_android_view.dart
@@ -1,18 +1,19 @@
 import 'package:bilibili_getx/core/system/system_preferred_orientations/system_preferred_orientations_util.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'flutter_android_logic.dart';
 
 ///显示Android原生界面（这里显示显示MPAndroidChart图表）
 ///在缩放的流畅度中，MPAndroidChart图表效果比flutter插件要好。特别是点数特别多的时候，flutter插件卡顿尤为明显
 class FlutterAndroidView extends StatelessWidget {
   static String routeName = "/flutter_android";
-
-  const FlutterAndroidView({super.key});
+  final logic = Get.find<FlutterAndroidLogic>();
+  final state = Get.find<FlutterAndroidLogic>().state;
 
   @override
   Widget build(BuildContext context) {
-    // final logic = Get.find<FlutterAndroidLogic>();
-    // final state = Get.find<FlutterAndroidLogic>().state;
     Widget platformView() {
       if (defaultTargetPlatform == TargetPlatform.android) {
         return const AndroidView(viewType: 'flutter_android_view');
@@ -25,7 +26,7 @@ class FlutterAndroidView extends StatelessWidget {
         title: const Text("Android原生"),
       ),
       body: WillPopScope(
-        onWillPop: () async{
+        onWillPop: () async {
           SystemPreferredOrientationsUtil.setVertical();
           return true;
         },
