4ebea12f2f08e37ce65e92118fe01c7b8dd62289
wangyangjun <2622504692@qq.com>
2023-07-15 15:44:47 +0800

    提交

--- lib/ui/pages/functions/canvas_paint_study/canvas006/paper.dart
+++ lib/ui/pages/functions/canvas_paint_study/canvas006/paper.dart
@@ -26,7 +26,8 @@ class PaperPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    // TODO: implement paint
+    _drawGrid(canvas, size);
+    drawCircle(canvas, size);
   }
 
   @override
@@ -39,7 +40,38 @@ class PaperPainter extends CustomPainter {
       canvas.translate(0, step);
     }
     canvas.restore();
+  }
 
+  void _drawGrid(Canvas canvas, Size size) {
+    _drawBottomRight(canvas, size);
     canvas.save();
+    canvas.scale(1, -1);
+    _drawBottomRight(canvas, size);
+    canvas.restore();
+    canvas.scale(-1, 1);
+    canvas.save();
+    _drawBottomRight(canvas, size);
+    canvas.restore();
+    canvas.scale(-1, -1);
+    canvas.save();
+    _drawBottomRight(canvas, size);
+    canvas.restore();
+  }
+
+  void drawCircle(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = Colors.blue;
+    canvas.translate(size.width / 2, size.height / 2);
+    canvas.drawCircle(Offset(0, 0), 50, paint);
+    canvas.drawLine(
+      Offset(20, 20),
+      Offset(50, 50),
+      paint
+        ..color = Colors.red
+        ..strokeWidth = 5
+        ..strokeCap = StrokeCap.round
+        ..style = PaintingStyle.stroke,
+    );
   }
 }
---
+++ lib/ui/pages/functions/canvas_paint_study/canvas007/paper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Paper extends StatelessWidget {
+  const Paper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
